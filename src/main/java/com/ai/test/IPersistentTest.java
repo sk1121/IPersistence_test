cb74dd26ab9ff1e9ba4d1bce396eb3f151207402
songkang <songkang@asiainfo.com>
2020-08-07 21:42:54 +0800

    读取Configuration

---
+++ src/main/java/com/ai/test/IPersistentTest.java
@@ -0,0 +1,15 @@
+package com.ai.test;
+
+import com.ai.io.Resources;
+
+/**
+ * @author songkang
+ * @Date 2020/8/7
+ */
+public class IPersistentTest {
+
+
+    public void test() {
+        Resources.getResourceAsSteam("sqlMapConfig.xml");
+    }
+}
